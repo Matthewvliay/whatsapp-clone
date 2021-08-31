969c3121957556c6d9e3f913f28e9cbcbcc3c323
Matthew <mvilay02@stanford.edu>
2021-08-31 09:36:33 -0500

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCqzFsTICCuvjuRQ7jAHT1_FdNa_XgwJr4",
+    authDomain: "whatsapp-mern-ede89.firebaseapp.com",
+    projectId: "whatsapp-mern-ede89",
+    storageBucket: "whatsapp-mern-ede89.appspot.com",
+    messagingSenderId: "774542002593",
+    appId: "1:774542002593:web:c001b535e0d53cf6e763e0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
